077d22a67f301ee113b595bae23ec97dea15e093
Numan Ahmed <mdnmnahmed@gmail.com>
2021-06-13 08:52:11 +0530

    Index, App & Routing added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { ToastContainer, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import SignUp from './Components/AuthComponents/SignUp/SignUp';
+import SignIn from './Components/AuthComponents/SignIn/SignIn';
+import Header from './Components/Header/Header';
+import { Auth } from './firebase';
+import Home from './Components/Home/Home';
+import ErrorNotFound from './Components/ErrorNotFound/ErrorNotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [user, setUser] = useState(null);
+
+    /**
+     * Checking user's Authorization
+     */
+    useEffect(() => {
+        Auth.onAuthStateChanged(user => user ? setUser(user) : setUser(null));
+    }, []);
+
+    return (
+        <>
+            <BrowserRouter >
+                {user && <Header />}
+
+                <Switch>
+                    <Route path='/' exact>
+                        <Home />
+                    </Route>
+                    <Route path='/signup'>
+                        <SignUp />
+                    </Route>
+                    <Route path='/signin'>
+                        <SignIn />
+                    </Route>
+                    <Route>
+                        <ErrorNotFound />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                transition={Zoom}
+            />
+        </>
+    );
 }
 
 export default App;
