d0a71a70b1106a670878e35aae3ac875f951c991
Numan Ahmed <mdnmnahmed@gmail.com>
2021-06-14 21:57:53 +0530

    Save notes into DB

--- src/Components/Journals/AddNote/AddNote.js
+++ src/Components/Journals/AddNote/AddNote.js
@@ -1,19 +1,43 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Grid, Container, Button } from '@material-ui/core';
 import BaseLayout from '../../Layouts/BaseLayout';
 import useStyles from './addNoteStyles';
-import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import FetchAPIData from '../../../helpers/FetchAPIData';
+import { useHistory } from 'react-router-dom';
 
 const AddNote = () => {
     const classes = useStyles();
-    const dispatch = useDispatch();
-
+    const history = useHistory();
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
 
-    
+    const addNoteHandler = async (event) => {
+        event.preventDefault();
+
+        if (!title || !description) {
+            toast.error('Enter Title & Description');
+            return;
+        }
+
+        try {
+            const newNoteData = {
+                headlines: title,
+                description
+            }
+
+            const noteResponse = await FetchAPIData('post', '/home-view', newNoteData);
+            if (noteResponse.data.status == 'OK') {
+                toast(title + ' - Note Saved');
+                history.push('/');
+            } else {
+                throw 'Error with Database'
+            }
+
+        } catch (error) {
+            toast.error('Failed to Add Note, ', error);
+        }
+    }
 
     return (
         <>
@@ -30,16 +54,12 @@ const AddNote = () => {
                                 value={description}
                                 onChange={(event) => setDescription(event.target.value)}
                             />
-
                             <Grid container spacing={0} direction="column" alignItems="center" justify="center" style={{ paddingTop: '50%' }}>
                                 <Button type="submit" variant="container" color="" className={classes.submit}>
                                     Save
                                 </Button>
                             </Grid>
                         </form>
-
-
-
                     </Grid>
                 </Container>
             </BaseLayout>
